6515b56dbea84164bd747899014978d64bdccc16
Brajesh <prajapatibrajesh003@gmail.com>
2025-10-03 23:39:54 +0530

    tasks setup completed

--- AdvancedJpa.java
+++ AdvancedJpa.java
@@ -67,3 +67,55 @@ Page<Student> page = studentRepo.findByDepartment("CS", pageable);
 
 System.out.println("Total Pages: " + page.getTotalPages());
 System.out.println("Total Elements: " + page.getTotalElements());
+
+
+// direct sorting
+List<Student> findAll(Sort sort);
+
+studentRepo.findAll(Sort.by("name").descending());
+
+Pageable pageable = PageRequest.of(0, 5, Sort.by("age").descending().and(Sort.by("name")));
+
+
+@Query("SELECT s FROM Student s WHERE s.id > :lastId ORDER BY s.id ASC")
+List<Student> findNextPage(@Param("lastId") Long lastId, Pageable pageable);
+
+
+
+
+// 3 Oct 2k25
+
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+    Page<Student> findByDepartment(String dept, Pageable pageable);
+}
+
+Pageable pageable = PageRequest.of(0, 10, Sort.by("name").ascending());
+Page<Student> page = studentRepo.findByDepartment("cs", pageable);
+
+System.out.println("Total pages: " + page.getTotalPages());
+System.out.println("Total elements: " + page.getTotalElements());
+
+
+class Learn{
+    void learn(){
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("name").ascending());
+        Page<Student> page = studentRepo.findByDepartment("cs", pageable);
+
+        System.out.println("Total pages: "+ page.getTotalPages());
+        System.out.println("Total elements: " + page.getTotalElements());
+
+
+        Pageable pageable = PageRequest.of(0, 5, Sort.by("name").ascending().and(Sort.by("age").descending()));
+
+
+        @Query("SELECT s FROM Student s WHERE s.id > :lastId ORDER BY s.id ASC")
+        List<Student> findNextPage(@Param("lastId") Long lastId, Pageable pageable); // much faster for infinite scroll
+
+
+        @Query("SELECT e FROM Employee e WHERE e.dept = :dept")
+        Page<Employee> findByDept(@Param("dept") String dept, Pageable pageable);
+
+    }
+}
