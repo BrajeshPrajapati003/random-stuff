1832802fc4e744c6a938373da7e9834db1ea8578
Brajesh <prajapatibrajesh003@gmail.com>
2025-10-09 21:45:02 +0530

    Fixed merge issues after reviewing with git diff

---
+++ scenario6/fitinspiration/backend/src/main/java/com/learn/fitinspiration/entity/User.java
@@ -0,0 +1,19 @@
+package scenario6.fitinspiration.backend.src.main.java.com.learn.fitinspiration.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    private Long id;
+    
+}
