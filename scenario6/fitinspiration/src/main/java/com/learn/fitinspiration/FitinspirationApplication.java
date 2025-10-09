aa418fbb96baf6651e621082e28b9ea9fb8b9044
Brajesh <prajapatibrajesh003@gmail.com>
2025-10-09 13:51:20 +0530

    starting scenario6

---
+++ scenario6/fitinspiration/src/main/java/com/learn/fitinspiration/FitinspirationApplication.java
@@ -0,0 +1,13 @@
+package com.learn.fitinspiration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitinspirationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitinspirationApplication.class, args);
+	}
+
+}
---
+++ scenario6/fitinspiration/src/test/java/com/learn/fitinspiration/FitinspirationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.fitinspiration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitinspirationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
