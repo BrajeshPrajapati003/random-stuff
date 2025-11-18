cc678a1f48144db3aa948182f69bcf3f4d01a22f
Brajesh <prajapatibrajesh003@gmail.com>
2025-11-18 23:34:58 +0530

    security topic-8 completed

---
+++ next/animations/motion-1/.next/dev/static/chunks/next_animations_motion-1_app_page_tsx_875e0a83._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/dde30_b60154b6._.js"
+],
+    source: "dynamic"
+});
