0172e00b0d039c0b61b6d57f84424ad1d27984f3
Brajesh <prajapatibrajesh003@gmail.com>
2025-12-22 01:36:25 +0530

    hospital db problems completed

--- learn.sql
+++ learn.sql
@@ -806,4 +806,29 @@ Drop the last digit of the integer.
         CONCAT(ROUND(SUM(gender = 'M') / CAST(COUNT(*) AS float), 4) * 100, '%')
     FROM patients;
 
--- 
+-- We need a breakdown for the total amount of admissions each doctor has started each year. Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
+    SELECT
+        d.doctor.id,
+        CONCAT(d.first_name, ' ', d.last_name) AS doctor_name,
+        d.specialty,
+        YEAR(a.admission_date) AS selected_year,
+        COUNT(*) AS total_admissions
+    FROM doctors d
+    JOIN admissions a 
+    ON a.attending_doctor_id = d.doctor_id
+    GROUP BY d.doctor_id, selected_year;
+---------------------------
+    SELECT
+        d.doctor_id,
+        CONCAT(d.first_name, ' ', d.last_name) AS doctor_name,
+        d.specialty,
+        YEAR(a.admission_date) AS selected_year,
+        COUNT(*) AS total_admissions
+    FROM doctors d 
+    LEFT JOIN admissions a
+        ON d.doctor_id = a.attending_doctor_id
+    GROUP BY
+        doctor_name,
+        selected_year
+    ORDER BY doctor_id, selected_year;
+
