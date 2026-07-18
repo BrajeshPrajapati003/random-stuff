287d52a9e459154de602c651a201ac29ecf2018f
Brajesh <prajapatibrajesh003@gmail.com>
2026-07-18 23:05:56 +0530

    practicing react

--- react/my-project/src/practice/recall.jsx
+++ react/my-project/src/practice/recall.jsx
@@ -18,3 +18,292 @@ const promise = new Promise((res, rej) => {
     }
 });
 
+promise
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+const promise2 = new Promise((res, rej)=> {
+    const success = Math.random() > 0.8;
+    if(success){
+        res("Data received");
+    } else{
+        rej("Error occurred");
+    }
+});
+
+promise
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+
+const res = await fetch("https://api.example.com/users");
+const data = res.json();
+
+const res2 = await fetch("url");
+const data2 = res.json();
+
+
+await fetch("/api/users",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name: "Dev" })
+});
+
+
+await fetch("/api/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name: "Dev" })
+});
+
+
+try{
+    const res = await fetch(url);
+    if(!res.ok){
+        throw new Error("API Failed");
+    }
+    const data = res.json();
+}catch(err){
+    console.error(err);
+}
+
+try{
+    const res = await fetch(url);
+    if(!res.ok){
+        throw new Error("Api failed");
+    }
+    const data = res.json();
+}catch(err){
+    console.error(err);
+}
+
+
+
+import {useEffect, useState} from "react";
+
+function Users1(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=> {
+        const fetchUsers = async ()=> {
+            try{
+                setLoading(true);
+                const res = await fetch(url);
+
+                if(!res.ok){
+                    throw new Error("Api failed");
+                }
+
+                const data = await res.json();
+                setUsers(data);
+            }catch(err){
+                setError(err);
+            }finally{
+                setLoading(false);
+            }
+        }
+
+        fetchUsers();
+    }, []);
+
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error: {error}</p>
+
+    return (
+        <ul>
+            {users.map(user => {
+                <li key={user.id}>{user.name}</li>
+            })}
+        </ul>
+    );
+}
+
+function Users2(){
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        
+        const fetchUsers = async ()=> {
+            try{
+                setLoading(true);
+                const res = await fetch(url);
+                if(!res.ok){
+                    throw new Error("Api failed");
+                }
+                const data = await res.json();
+                setUsers(data);
+            }catch(err){
+                setError(err.message);
+            }finally{
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    },[])
+
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error: {error}</p>
+
+    return (
+        <ul>
+            {users.map(user => 
+                <li key={user.id}>{user.name}</li>
+            )}
+        </ul>
+    );
+}
+
+
+function User3(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=> {
+        const controller = new AbortController();
+        const fetchData = async ()=> {
+            try{
+                setLoading(true);
+                const res = await fetch(url, {
+                    signal: controller.signal
+                });
+                
+                const data = await res.json();
+                setUsers(data);
+            }catch(err){
+                if(err.name !== "AbortError"){
+                    setError(err);
+                }
+            }finally{
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+        return ()=> controller.abort();
+    }, [])
+
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>Error: {error.message}</p>
+
+    return (
+        <ul>
+            {users.map(user => {
+                return <li key={user.id}>{user.name}</li> // due to {} jsx expects an explicit return; else use ()
+            })}
+        </ul>
+    )
+}
+
+                            /** MEMORIZE THE PATTERN */
+function User4(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=> {
+
+        const controller = new AbortController();
+        const fetchData = async ()=> {
+            try{
+                setLoading(true);
+                const res = await fetch(url, {
+                    signal: controller.signal
+                });
+
+                if(!res.ok){
+                    throw new Error("Fetch Failed");
+                }
+
+                const data = await res.json();
+                setUsers(data);
+
+            }catch(err){
+                if(err.name !== "AbortError"){
+                    setError(err);
+                }
+            }finally{
+                if(!controller.signal.aborted){
+                    setLoading(false);
+                }
+            }
+        };
+
+        fetchData();
+        return ()=> controller.abort();
+    }, []);
+
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error: {error.message}</p>
+
+    return (
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+    )
+}
+
+
+function User5(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(()=> {
+
+        const controller = new AbortController();
+        const fetchData = async ()=> {
+            try{
+                setLoading(true);
+                const res = await fetch(url, {
+                    signal: controller.signal
+                });
+
+                if(!res.ok){
+                    throw new Error("Failed to Fetch users!");
+                }
+
+                const data = res.json();
+                setUsers(data);
+            }catch(err){
+                if(err.name !== "AbortError"){
+                    setError(err);
+                }
+            }finally{
+                if(!controller.signal.aborted){
+                    setLoading(false);
+                }
+            }
+        };
+
+        fetchData();
+        return ()=> controller.abort();
+    }, [])
+
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error: {error.message}</p>
+
+    return (
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+    )
+}
+
+
